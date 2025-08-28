10a4ada70987239ab5f7f591363e4d2b512db2ad
Hanif Abdillah Alatas <hanifabdillah2212@gmail.com>
2025-08-28 14:44:56 +0700

    Delete proyek1 directory

--- proyek1/hanif_project1.java
+++
@@ -1,8 +0,0 @@
-class Main {
-    public static void main(String[]args) {
-        System.out.println("Nama: Hanif");
-        System.out.println("NPM: 2420506041");
-        System.out.println("Alamat: Magelang");
-        System.out.println("No_HP: 088709749940");
-    }
-}
